1562dc55a6adcf02c150e3ca4625552f49bb31d7
vinay kumar <kumarvinay07318@gmail.com>
2024-07-22 10:57:31 +0530

    hlw

--- Routes/avatarRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import { avatarController } from '../controller/avatarController.js';
-const router = express.Router();
-
-router.post('/upload',avatarController)
-
-export {router}
